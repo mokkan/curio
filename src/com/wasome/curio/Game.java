2eea50a3ea7d6e5047d80485b1f37caaa7cdb970
Michael Swiger <mokkan@wasome.com>
2013-10-05 20:05:20 -0700

    Switch from tabs to spaces
    Spaces are objectively better. :)

--- src/com/wasome/curio/Game.java
+++ src/com/wasome/curio/Game.java
@@ -5,44 +5,44 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class Game implements ApplicationListener {
 
-	@Override
-	public void create() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void create() {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void render() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public static void main(String[] args) {
-		new LwjglApplication(new Game(), "Curio", 640, 480, false);
-	}
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    public static void main(String[] args) {
+        new LwjglApplication(new Game(), "Curio", 640, 480, false);
+    }
 
 }
