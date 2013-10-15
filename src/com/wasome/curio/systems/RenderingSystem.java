b0e78bbb779c98b333be7675476993fe38a9d74a
Michael Swiger <mokkan@wasome.com>
2013-10-15 15:57:20 -0700

    Remove extraneous import

--- src/com/wasome/curio/systems/RenderingSystem.java
+++ src/com/wasome/curio/systems/RenderingSystem.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Vector3;
 import com.wasome.curio.components.Position;
 
 public class RenderingSystem extends EntityProcessingSystem {
