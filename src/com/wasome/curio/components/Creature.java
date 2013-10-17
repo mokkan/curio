ca9b44df4f87bae51b5d0c567cc809d82723c810
Michael Swiger <mokkan@wasome.com>
2013-10-16 17:22:56 -0700

    Add Creature component
    This component keeps track of the status of the creature (like if the
    creature is walking, jumping, climbing, etc).

---
+++ src/com/wasome/curio/components/Creature.java
@@ -0,0 +1,26 @@
+package com.wasome.curio.components;
+
+import com.artemis.Component;
+
+public class Creature extends Component {
+    
+    public static final int STATUS_IDLE = 1;
+    public static final int STATUS_WALKING = 2;
+    public static final int STATUS_JUMPING = 3;
+    public static final int STATUS_CLIMBING = 4;
+    
+    private int status;
+    
+    public Creature() {
+        status = STATUS_IDLE;
+    }
+    
+    public int getStatus() {
+        return status;
+    }
+    
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+}
