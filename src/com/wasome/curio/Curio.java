46c287988d1030033871bf93928139c3d8bc032d
Michael Swiger <mokkan@wasome.com>
2013-10-20 21:32:16 -0700

    Disable power-of-two texture requirements

--- src/com/wasome/curio/Curio.java
+++ src/com/wasome/curio/Curio.java
@@ -3,6 +3,7 @@ package com.wasome.curio;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Curio extends Game {
 
@@ -15,6 +16,9 @@ public class Curio extends Game {
     }
     
     public static void main(String[] args) {
+        // Disable power of two texture requirements
+        Texture.setEnforcePotImages(false);
+        
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "Curio";
         cfg.width = 1280;
