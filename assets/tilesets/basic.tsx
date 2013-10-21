03cf780e66963c5ba0dc5c6f03f9115a89bfe2ae
Michael Swiger <mokkan@wasome.com>
2013-10-20 21:33:17 -0700

    Remove "basic" tileset that was used for testing

--- assets/tilesets/basic.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Basic" tilewidth="16" tileheight="16">
- <image source="basic.png" width="32" height="16"/>
- <tile id="0">
-  <properties>
-   <property name="solid" value=""/>
-  </properties>
- </tile>
-</tileset>
